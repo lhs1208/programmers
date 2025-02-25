ecfac18bd3b1e67731931909f626d95037d13213
lhs <mymail777@naver.com>
2025-02-25 21:59:40 +0900

    test commit

---
+++ src/sample5.java
@@ -0,0 +1,16 @@
+package src;
+
+public class sample5 {
+
+
+    public static int solution(){
+
+
+        return 2323;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution());
+    }
+
+}
